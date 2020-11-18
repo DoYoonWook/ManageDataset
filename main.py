96ae6ea8987ce4023b08e45a1049597eec56841f
Sette <40222247+Sette-Tyx@users.noreply.github.com>
2020-11-18 16:53:04 +0800

    2020-11-18

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import xml.etree.ElementTree as ET
 from xml.dom.minidom import parse
 from xml.etree.ElementTree import ElementTree, Element
 
-
+def read_xml(annoPath):
+    tree = ET.parse(annoPath)
+    root = tree.getroot()
+    xml_content = open(annoPath).read()
+    return tree, root, xml_content
 def test():
     imgRootPath = './test_voc/images/'
     annoRootPath = './test_voc/annotations/'
@@ -13,47 +17,41 @@ def test():
     #     root = domTree.documentElement
 
     # 从xml文件中读取，使用getroot()获取根节点，得到的是一个Element对象
-    tree = ET.parse(annoRootPath + './100009.xml')
-    print('tree', type(tree))
-    root = tree.getroot()
-    print('root', type(root))
-    for element in root.findall('object'):
-        tag = element.tag
-        attrib = element.attrib
-        text = element.find('name').text
-        print(tag, attrib, text)
+    # tree = ET.parse(annoRootPath + './100009.xml')
+    # print('tree', type(tree))
+    # root = tree.getroot()
+    # print('root', type(root))
 
-    for obj in root.iter('object'):
-        root.remove(obj)
-        print(obj[0].text)
-        # root.remove(obj)
+    tree, root, xml_content = read_xml(annoRootPath + './100009.xml')
+    object_list = root.findall('object')
+    count = object_list
+
+    for i in range(0, count):
+
+
+
+
+
+
+    # 移除object
+    # for element in root.findall('object'):
+    #     tag = element.tag
+    #     attrib = element.attrib
+    #     text = element.find('name').text
+    #     print(tag, attrib, text)
+    #     root.remove(element)
+    # object_list = root.findall('object')
+    # print(len(object_list))
+    # for i in range(0, len(object_list)):
+
+
+
+    # for obj in root.iter('object'):
+    #     root.remove(obj)
+    #     print(obj[0].text)
+    #     # root.remove(obj)
 
     tree.write('test.xml')
-
-
-    # print(root[0][0].text)  # 子节点是嵌套的，我们可以通关索引访问特定的子节点
-
-
-
-
-
-
-
-
-
-
-
-    # print(rootNode.nodeName)
-
-    # objects = root.getElementsByTagName('object')
-    # print('objects', type(objects))
-    # for ob in objects:
-
-        # print(nameList[0].childNodes[0].data)
-        # bndbox = nameList.getchildren()
-        # print(bndbox)
-
-
 
 
 if __name__ == '__main__':
